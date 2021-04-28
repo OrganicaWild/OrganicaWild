51274a494e4a8e4cb5188f4be161e8bfd759598c
Christoph Vögele <christoph228@gmail.com>
2021-04-28 12:32:19 +0200

    comment area type assignment

--- Assets/Scripts/Framework/Pipeline/PipeLineSteps/AreaTypeAssignmentStep.cs
+++ Assets/Scripts/Framework/Pipeline/PipeLineSteps/AreaTypeAssignmentStep.cs
@@ -21,31 +21,39 @@ namespace Assets.Scripts.Framework.Pipeline.PipeLineSteps
 
         public GameWorld Apply(GameWorld world)
         {
+            //get all areas
             IEnumerable<Area> areas = world.Root.GetAllChildrenOfType<Area>();
+            
             Vector2Comparer comparer = new Vector2Comparer();
             List<Area> areaList = areas.ToList();
 
+            //sort areas based on distance to origin of centroid
             areaList.Sort((area1, area2) =>
                 comparer.Compare(area1.Shape.GetCentroid(), area2.Shape.GetCentroid()));
 
             Area startArea = areaList.First();
             Area endArea = areaList.Last();
 
+            //remove non typed areas
             world.Root.RemoveChild(startArea);
             world.Root.RemoveChild(endArea);
 
             TypedArea startTypedArea = new TypedArea(startArea.Shape, null, -1);
-            startTypedArea.AddChild(new Landmark(new OwPoint(startTypedArea.Shape.GetCentroid() + Vector2.one), null));
             TypedArea endTypedArea = new TypedArea(endArea.Shape, null, int.MaxValue);
-            endTypedArea.AddChild(new Landmark(new OwPoint(endTypedArea.Shape.GetCentroid()), null));
+            
+            //landmarks used for debugging
+            //startTypedArea.AddChild(new Landmark(new OwPoint(startTypedArea.Shape.GetCentroid() + Vector2.one), null));
+            //endTypedArea.AddChild(new Landmark(new OwPoint(endTypedArea.Shape.GetCentroid()), null));
 
+            //add typed areas
             world.Root.AddChild(startTypedArea);
             world.Root.AddChild(endTypedArea);
 
+            //remove non typed areas from list, so that they are not handled as in-between areas
             areaList.Remove(startArea);
             areaList.Remove(endArea);
 
-            //in between areas
+            //assign type to in-between areas
             foreach (Area area in areaList)
             {
                 world.Root.RemoveChild(area);
