b07fe62cffdef566291ac86cd1b2728a02d721fc
Christoph Vögele <christoph228@gmail.com>
2021-05-04 00:10:22 +0200

    rename class border ca rule to file name

--- Assets/Scripts/Demo/Pipeline/TrialPipeline/TrialCellularAutomata/Rules/ScriptableObjects/BecomeBorderRule.cs
+++ Assets/Scripts/Demo/Pipeline/TrialPipeline/TrialCellularAutomata/Rules/ScriptableObjects/BecomeBorderRule.cs
@@ -1,26 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Assets.Scripts.Demo.Pipeline.TrialPipeline.TrialCellularAutomata;
 using Assets.Scripts.Framework.Cellular_Automata.Polymorphic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "borderRule", menuName = "Rules/BorderRule", order = 0)]
-public class BorderRule : TrialUpdateRule
+namespace Demo.Pipeline.TrialPipeline.TrialCellularAutomata.Rules.ScriptableObjects
 {
-    public int maxPathNeighbours;
-    public int maxBackGroundNeighbours;
-    
-    public override TrialCellState ApplyTo(TrialCell cell)
+    [CreateAssetMenu(fileName = "borderRule", menuName = "Rules/BorderRule", order = 0)]
+    public class BecomeBorderRule : TrialUpdateRule
     {
-        int pathNeighbours = cell.Neighbors.Sum(n => n.CurrentState == TrialCellState.Path ? 1 : 0);
-        int backgroundNeighbours = cell.Neighbors.Sum(n => n.CurrentState == TrialCellState.Background ? 1 : 0);
-
-        if (pathNeighbours >= maxPathNeighbours && backgroundNeighbours >= maxBackGroundNeighbours)
+        public int maxPathNeighbours;
+        public int maxBackGroundNeighbours;
+    
+        public override TrialCellState ApplyTo(TrialCell cell)
         {
-           return TrialCellState.PathBackgroundBorder;
-        }
+            int pathNeighbours = cell.Neighbors.Sum(n => n.CurrentState == TrialCellState.Path ? 1 : 0);
+            int backgroundNeighbours = cell.Neighbors.Sum(n => n.CurrentState == TrialCellState.Background ? 1 : 0);
 
-        return cell.CurrentState;
+            if (pathNeighbours >= maxPathNeighbours && backgroundNeighbours >= maxBackGroundNeighbours)
+            {
+                return TrialCellState.PathBackgroundBorder;
+            }
+
+            return cell.CurrentState;
+        }
     }
 }
