9e7123ae020a1cbdf598ff1aa266bdf69145b26e
Christoph Vögele <christoph228@gmail.com>
2021-05-04 14:58:06 +0200

    change generic landmark prefabs

--- Assets/Scripts/Framework/Pipeline/ThemeApplicator/Recipe/PointPrefabRecipe.cs
+++ Assets/Scripts/Framework/Pipeline/ThemeApplicator/Recipe/PointPrefabRecipe.cs
@@ -7,20 +7,21 @@ namespace Framework.Pipeline.ThemeApplicator.Recipe
     [CreateAssetMenu(fileName = "PointPrefabRecipe", menuName = "Pipeline/PointPrefabRecipe", order = 0)]
     public class PointPrefabRecipe :  GameWorldObjectRecipe
     {
-        public GameObject landmarkPrefab;
+        public GameObject[] landmarkPrefabs;
+        public Vector3 minScale;
+        public Vector3 maxScale;
 
         public override GameObject Cook(IGameWorldObject individual)
         {
             OwPoint point = individual.Shape as OwPoint;
+            GameObject landmarkPrefab = landmarkPrefabs[(int) (random.NextDouble() * (landmarkPrefabs.Length))];
             GameObject instantiate = Instantiate(landmarkPrefab);
             instantiate.transform.position =
                 landmarkPrefab.transform.position + new Vector3(point.Position.x, 0,point.Position.y);
-            instantiate.transform.rotation = landmarkPrefab.transform.rotation;
-
-            float scale = (float) (random.NextDouble() / 5f);
+            instantiate.transform.rotation = landmarkPrefab.transform.rotation ;
+            Vector3 scale = Vector3.Lerp(minScale, maxScale, (float) random.NextDouble());
+            instantiate.transform.localScale = scale;
             
-            instantiate.transform.localScale = new Vector3(scale, scale, scale);
-
             return instantiate;
         }
     }
