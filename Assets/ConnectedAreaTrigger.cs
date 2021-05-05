0722ce22d387343909640e33ec2c65983588945c
Christoph Vögele <christoph228@gmail.com>
2021-05-05 17:05:34 +0200

    add connected area trigger

---
+++ Assets/ConnectedAreaTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ConnectedAreaTrigger : MonoBehaviour
+{
+    public int partOfGroupX;
+    public static Dictionary<int, bool> groupsActivated = new Dictionary<int, bool>();
+
+    public GameObject toSpawn;
+    
+    private void Start()
+    {
+        if (!groupsActivated.ContainsKey(partOfGroupX))
+        {
+            groupsActivated.Add(partOfGroupX, false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (groupsActivated[partOfGroupX] == false)
+        {
+            groupsActivated[partOfGroupX] = true;
+            Instantiate(toSpawn);
+        }
+    }
+}
